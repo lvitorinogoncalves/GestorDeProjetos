8b659181711948869f98f3d355b540762d4a8af0
lvitorinogoncalves <lvitorinogoncalves@gmail.com>
2024-10-23 00:30:40 -0300

    Ajuste na entidade com os atributos corretos

--- src/main/java/com/gestorDePorjetos/model/Membro.java
+++ src/main/java/com/gestorDePorjetos/model/Membro.java
@@ -14,11 +14,6 @@ public class Membro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "id_projeto", nullable = false)
-    private Projeto projeto;
-
-    @ManyToOne
-    @JoinColumn(name = "id_pessoa", nullable = false)
-    private Pessoa pessoa;
+    private String nome;
+    private String atribuicao;
 }
