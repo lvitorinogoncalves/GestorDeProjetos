a883d5b71de7fa9ba33e4c8de3dd6016bd2d4d7d
Lucas Vitorino Gonçalves <lvitorinogoncalves@gmail.com>
2024-10-22 23:39:56 -0300

    Criação do projeto

---
+++ src/main/java/com/gestorDePorjetos/GestorDeProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.gestorDePorjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeProjetosApplication.class, args);
+	}
+
+}
